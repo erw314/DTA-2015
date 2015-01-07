3b716a5f7360d97351b40e3a7931df1ab1118a41
Simon Zheng <simonxbzheng@gmail.com>
2015-01-07 00:06:06 -0500

    created Trip class

--- driver_data.py
+++ driver_data.py
@@ -1,9 +1,16 @@
 import json
 
+# 
+
 def getAllDriverData():
-	file = open("~/driverData.json", "r")
+	file = open('~/driverData.json', 'r')
 	return json.load(file)
 
 # Returns all of input driver's drives, which are 
-getAllDrivesFromDriver()
+def getAllTripsFromDriver():
+	return None
 
+# Returns specified drives from a driver
+def getTripsFromDriver(driver_number, trip_indices):
+
+# make Trip object
---
+++ trip.py
@@ -0,0 +1,15 @@
+class Trip:
+	def __init__(self, driver, coordinates):
+		self.driver = driver
+		self.coordinates = coordinates
+		self.rotated_coordinates # ensures that trip ends on x axis
+
+	def plot_trip():
+
+	def speed():
+
+	def absolute_acceleration():
+
+
+
+
