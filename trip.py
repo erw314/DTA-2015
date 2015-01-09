a80e3310e7144972defa3ab02593c3f19ae6ac2a
Simon Zheng <simonxbzheng@gmail.com>
2015-01-08 21:04:43 -0500

    halfway done with rotation in Trip

--- trip.py
+++ trip.py
@@ -1,5 +1,6 @@
 import math
 import matplotlib.pyplot as plt
+import numpy as np
 
 class Trip:
 	# coordinates - list of tuples of floats, where first is (0,0)
@@ -34,10 +35,27 @@ class Trip:
 	def interval_absolute_acceleration(self):
 		return None
 
-	# Rotate coordinates of a trip so that the first 
-	def rotate_coordinates(self, coordinates, angle):		
-		return None
+	# Rotate counterclockwise the coordinates of a trip by a specified angle in radians
+	def rotate_coordinates(self, angle):	
 
+		# Rotate the point (x,y) counterclockwise by theta radians
+		def rotate(x, y, theta):
+			point = np.matrix([[x], [y]])
+			rotation_matrix = [[math.cos(theta), -math.sin(theta)], [math.sin(theta), math.cos(theta)]]
+			rotated_point = rotation_matrix * point
+			return (rotated_point.item(0,0), rotated_point.item(1,0))
+
+		return map(lambda coord : rotate(coord[0], coord[1], angle), self.coordinates)	
+			
+
+
+
+
+'''
+		last_x, last_y = self.x_coordinates[-1], self.y_coordinates[-1]	
+		# angle to rotate the point (last_x, last_y) counterclockwise so that the point lies on the positive x axis
+		angle_to_x_axis = -math.atan2(last_y, last_x)
+'''
 
 coordinates = [(0,0), (3,4), (4,5)]
 trip = Trip(0, coordinates)
@@ -50,4 +68,5 @@ print trip.interval_distance(1,2)
 print trip.total_distance()
 print trip.interval_average_speed(1,2)
 print trip.total_average_speed()
-print trip.plot()
+#print trip.plot()
+print trip.rotate_coordinates(math.pi/3)
