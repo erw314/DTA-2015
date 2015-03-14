c0767156bdca357ae1e05031a85500cefd3302b0
Eric <wang.eric.r@gmail.com>
2015-03-14 17:13:41 -0400

    testing for distance comparison

--- data_processing.py
+++ data_processing.py
@@ -4,6 +4,7 @@ import numpy as np
 from sklearn import preprocessing
 import cPickle as pickle
 from trip import Trip
+import transform as tf
 import os
 
 
@@ -12,19 +13,19 @@ def extract_feature_vectors(trips):
     input list of n Trip objects
     returns list of n d-dimensional feature vectors
     '''
-	d = 5 # number of features
-	feature_vectors = []
-	for trip in trips:
-		feature_vector = [0 for i in range(d)]
-		feature_vector[0] = trip.max_speed()
-		feature_vector[1] = trip.total_average_speed()
-		feature_vector[2] = trip.max_absolute_acceleration()
-		feature_vector[3] = trip.total_absolute_acceleration()
-		feature_vector[4] = trip.max_abs_acceleration_after_stop()
+    d = 5 # number of features
+    feature_vectors = []
+    for trip in trips:
+        feature_vector = [0 for i in range(d)]
+        feature_vector[0] = trip.max_speed()
+        feature_vector[1] = trip.total_average_speed()
+        feature_vector[2] = trip.max_absolute_acceleration()
+        feature_vector[3] = trip.total_absolute_acceleration()
+        feature_vector[4] = trip.max_abs_acceleration_after_stop()
 
-		feature_vectors.append(feature_vector)
+        feature_vectors.append(feature_vector)
 
-	return feature_vectors
+    return feature_vectors
 
 
 def extract_normalized_feature_vectors(trips):
@@ -33,7 +34,7 @@ def extract_normalized_feature_vectors(trips):
     returns n by d numpy array with d normalized features for radial basis SVM
     normalized with mean 0, s.d. 1
     '''
-	return preprocessing.scale(extract_feature_vectors(trips))
+    return preprocessing.scale(extract_feature_vectors(trips))
     
 
 def get_false_trips(samples, not_driver):
@@ -44,7 +45,7 @@ def get_false_trips(samples, not_driver):
     all_false = []
     files = []
     for n in os.listdir('./'):
-        if n[:11] == 'driverData_' and n[11:] != str(int(not_driver)/57)+'.p' and n[11:] != 'small.p': 
+        if n[:11] == 'driverData_' and n[11:] != str(int(not_driver)/48)+'.p' and n[11:] != 'small.p': 
             files.append(n)
     print files
     data_file = np.random.choice(files)
@@ -58,7 +59,7 @@ def get_false_trips(samples, not_driver):
         trips = zip(np.random.choice(range(len(temp_drivers)), samples, replace=False), np.random.choice(range(200), samples, replace=False))
 
         for t in trips:
-            all_false.append(Trip(57*i+t[0], t[1], temp_drivers[t[0]][t[1]]))
+            all_false.append(Trip(48*i+t[0], t[1], temp_drivers[t[0]][t[1]]))
     except:
         del temp_drivers
         raise
@@ -88,7 +89,7 @@ def distance(trip1, trip2):
     if len(path1) < len(path2):
         for i in range(len(path2)-len(path1)):
             path1.append(path1[-1])
-    distance = sum([((path1[i][0]-path2[i][0])**2+(path1[i][1]-path2[i][1])**2)**0.5 for i in range(len(path1))])
+    distance = np.mean([((path1[i][0]-path2[i][0])**2+(path1[i][1]-path2[i][1])**2)**0.5 for i in range(len(path1))])
     path1 = tf.reflect(path1)
-    distanceR = sum([((path1[i][0]-path2[i][0])**2+(path1[i][1]-path2[i][1])**2)**0.5 for i in range(len(path1))])
+    distanceR = np.mean([((path1[i][0]-path2[i][0])**2+(path1[i][1]-path2[i][1])**2)**0.5 for i in range(len(path1))])
     return min(distance,distanceR)
